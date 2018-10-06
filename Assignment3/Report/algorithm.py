e946eef951a92cfb133fcc79dd6dae1e39ff5062
Zaymon Foulds Cook <generation281@gmail.com>
2018-10-06 16:47:44 +1000

    Added more to assignment 3 journal

--- Assignment3/Report/algorithm.py
+++ Assignment3/Report/algorithm.py
@@ -1,32 +1,32 @@
 def ConstructVCGreedy(Graph g, QuickSet vSet):
-	const int index <- index between 0 .. |V|
-	vSet.Insert(index)
+	index <- index between 0 .. |V|
+	vSet <-+ index
 
-	while(!g.IsVertexCover(vSet)):
+	while vSet is not vertex cover of g:
 		degrees <- g.GetDegreeList(vSet)
 		sort degrees
 		vSet <-+ highestDegreeNode[degrees]
 
 
-def MVC_Search(Graph g, int swapLimit, int operationsPerPhase, int iterationCap):
+ALGORITHM MVC_Search(Graph g, int swapLimit, int operationsPerPhase, int iterationCap):
 	QuickSet vSet <- QuickSet(g.vertices)
 	ConstructVCGreedy(g, vSet)
 
-	list<int> penalty <- list of -1, length = |V|
+	penalty <- list of -1, length = |V|
 	
 	iterations <- 0
-	bestCover <- vSet.Size()
+	bestCover <- |vSet|
 
 	while iterations < iterationCap:
 		for operation < operationsPerPhase:
-			const int depth <- random between 0..10
+			depth <- random between 0...10
 
 			// Reduce the number of nodes in the list
 			for i in range 0...depth:
-				u <- rand() % (vSet.Size() - 1)
+				u <- random between 0...|vSet|
 
-				while (!vSet.Contains(u)) {
-					u <- rand() % (vSet.Size() - 1)
+				while vSet does not contain u:
+					u <- random between 0...|vSet|
 				remove u from vSet
 				AddToPenalty(penalty, u)
 				UpdatePenalty(penalty)
@@ -42,8 +42,8 @@ def MVC_Search(Graph g, int swapLimit, int operationsPerPhase, int iterationCap)
 
 				index <- 0
 				while penalty[degree[index].node]:
-					index++
-					if index <- degree.size():
+					index <- index + 1
+					if index <- |degree|:
 						index <- random in range 0... |V| - |vSet|
 						break
 
@@ -53,57 +53,58 @@ def MVC_Search(Graph g, int swapLimit, int operationsPerPhase, int iterationCap)
 
 				if g is vertex cover break
 		if g is vertex cover:
-			if (vSet.Size() < bestCover) bestCover <- vSet.Size()
+			if (|vSet| < bestCover) bestCover <- |vSet|
+
 
 		// --- Stabalisation Phase
 		if odd iteration:
-			const v <- rand() % (vSet.Size() - 1)
+			v <- random between 0...|vSet|
 			vSet.Remove(v)
 
 		swapCount <- 0
 
-		// Get Back to a vertex Cover with swaps
-		while !g.IsVertexCover(vSet) and swapCount < swapLimit:
+		// Get Back to a vertex cover with swaps
+		while vSet is not vertex cover of g and swapCount < swapLimit:
 			degree <- g.GetDegreeList(vSet)
 			sort degree
 
 			index <- 0
 			while penalty[degree[index].node]:
-				index++
-				if index <- degree.size():
-					index <- rand() % (degree.size() - 1)
+				index <- index + 1
+				if index <- |degree|:
+					index <- random between 0...|degree|
 					break
 			vSet <-+ degree[index].node
 
 			while true:
-				const u <- random in range 0...|vSet|
+				u <- random in range 0...|vSet|
 				if penalty.at(u) > 0 continue
 
 			AddToPenalty(penalty, degree.at(index).second)
 			UpdatePenalty(penalty)
-			swaps--
+			swaps <- swaps - 1
 
 		if g is vertex cover:
-			if vSet.Size() < bestCover:
-				bestCover <- vSet.Size()
+			if |vSet| < bestCover:
+				bestCover <- |vSet|
 
 		if iteration is odd 
 			degree <- g.GetDegreeList(vSet)
 			sort degree
 			vSet <-+ one node from top 5 in degree
-		iterations++
+		iterations <- iterations + 1
 	return bestCover
 
 def SearchRunner(Graph g):
-	const maximumSwaps <- 100
-	const operations <- 50
-	const iterations <- 2
-	const restarts <- 50
+	maximumSwaps <- 100
+	operations <- 50
+	iterations <- 2
+	restarts <- 50
 
-	best <- std::numeric_limits<int>::max()
+	best <- max int
 
 	for i in range 0...restarts:
-		const score <- MVC_Search(g, maximumSwaps, operations, iterations)
+		score <- MVC_Search(g, maximumSwaps, operations, iterations)
 		if score < best:
 			best <- score
 	return best
